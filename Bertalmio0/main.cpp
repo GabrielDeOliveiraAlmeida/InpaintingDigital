005064d11dc9f58a170388282667a7b3999a0958
Gabriel de Oliveira Almeida <gabriel.doliveira.almeida@gmail.com>
2020-07-24 23:27:18 -0300

    Passei o dominio de retoque dentro de um vetor

--- Bertalmio0/main.cpp
+++ Bertalmio0/main.cpp
@@ -2,14 +2,6 @@
 #include "opencv2/opencv.hpp";
 #include <thread>  
 
-void perform_bertalmio_pde_inpainting_0(
-	cv::Mat& input_array, cv::Mat& mask_array, cv::Mat& output_array,
-	int* total_iters, int* total_inpaint_iters, int* total_anidiffuse_iters, int total_stages,
-	float* delta_ts, float* sensitivities, int diffuse_coef);
-
-void imshowOutput(std::string output_window_name, cv::Mat& output_array);
-
-
 enum { PIXEL_WHITE = 1, PIXEL_RED = 2 };
 typedef struct coord {
 	int i;
@@ -17,41 +9,51 @@ typedef struct coord {
 	int color;
 }Coord;
 
-using namespace cv;
+std::vector<Coord> create_mask(cv::Mat& mask); 
+void perform_bertalmio_pde_inpainting_0(
+	cv::Mat& input_array, std::vector<Coord>&  mask_array, cv::Mat& output_array,
+	int* total_iters, int* total_inpaint_iters, int* total_anidiffuse_iters, int total_stages,
+	float* delta_ts, float* sensitivities, int diffuse_coef);
+
+void imshowOutput(std::string output_window_name, cv::Mat& output_array);
+
+
 
 int main(int argc, char* argv[])
 {
 	/* name of the images */
-	std::string image_name = "simpleimg.png";
-	std::string mask_name = "simpleimg_mask.png";
+	std::string dir = "img/";
+	std::string name = "lena";
+	std::string image_name = dir + name + ".png";
+	std::string mask_name = dir + name + "_mask.png";
 	std::string window_name = image_name;
 	std::string mask_window_name = mask_name;
 	std::string output_window_name = "output_array";
 	
-	cv::Mat output_array;
-	
-	/* Load and normalize the image */
-	cv::Mat image_array = cv::imread(image_name,  cv::IMREAD_GRAYSCALE);
-	image_array.convertTo(image_array, CV_32FC1);
-	cv::normalize(image_array, image_array, 0, 1, cv::NORM_MINMAX, CV_32FC1);
 
-	/* Load the mask */
-	cv::Mat mask_array = cv::imread(mask_name, cv::IMREAD_GRAYSCALE);
+	cv::Mat output_array;
+
+
+	/* Load and normalize the image */
+	cv::Mat image_array = cv::imread(image_name);
+	image_array.convertTo(image_array, CV_32FC1);
+	cv::cvtColor(image_array, image_array, cv::COLOR_BGR2GRAY);
+	cv::normalize(image_array, image_array, 0, 1, cv::NORM_MINMAX, CV_32FC1);
+		
+
+	/* Load the mask and fill the vector*/
+	cv::Mat mask_array = cv::imread(mask_name);
+	std::vector<Coord> mask_data = create_mask(mask_array);
 	
-	/* Create output_array */
-	/*output_array.create(image_array.size(), CV_32FC1);
-	typedef unsigned char logical_type;*/
-	
-	
-	std::cout << image_array.channels();
 
 	/* Create the windows */
 	cv::namedWindow(window_name, cv::WINDOW_AUTOSIZE);
 	cv::namedWindow(mask_window_name, cv::WINDOW_AUTOSIZE);
 	cv::namedWindow(output_window_name, cv::WINDOW_AUTOSIZE);
-	
 
-	
+	/*
+		Bertalmio PDE Inpainting.	
+	*/
 	int total_iters[] = { 500 };
 	int total_inpaint_iters[] = { 6 };
 	int total_anidiffuse_iters[] = { 6 };
@@ -59,16 +61,17 @@ int main(int argc, char* argv[])
 	float delta_ts[] = { 0.02f };
 	float sensitivites[] = { 100 };
 	int diffuse_coef = 1;
+
 	perform_bertalmio_pde_inpainting_0(
-		image_array, mask_array, output_array,
+		image_array, mask_data, output_array,
 		total_iters, total_inpaint_iters, total_anidiffuse_iters, total_stages,
 		delta_ts, sensitivites, diffuse_coef);
 
 	/* Display the output */
-	/*cv::imshow(window_name, image_array);
+	cv::imshow(window_name, image_array);
 	cv::imshow(mask_window_name, mask_array);
 	cv::imshow(output_window_name, output_array);
-	cv::waitKey(0);*/
+	cv::waitKey(0);
 }
 
 void imshowOutput(std::string output_window_name, cv::Mat& output_array) {
@@ -77,7 +80,7 @@ void imshowOutput(std::string output_window_name, cv::Mat& output_array) {
 }
 
 void perform_bertalmio_pde_inpainting_0(
-	cv::Mat& input_array, cv::Mat& mask_array, cv::Mat& output_array,
+	cv::Mat& input_array, std::vector<Coord>& mask_array, cv::Mat& output_array,
 	int* total_iters, int* total_inpaint_iters, int* total_anidiffuse_iters, int total_stages,
 	float* delta_ts, float* sensitivities, int diffuse_coef) {
 
@@ -99,20 +102,10 @@ void perform_bertalmio_pde_inpainting_0(
 
 	/* Initialize output */
 	input_array.copyTo(output_array);
-
-	/*for (int row = 0; row < output_array.rows; row++) {
-		logical_type* mask_array_ptr = mask_array.ptr<logical_type>(row);
-		float* output_array_ptr = output_array.ptr<float>(row);
-		for (int col = 0; col < output_array.cols; col++) {
-			if ((mask_array_ptr[col] != 0)) {
-				output_array_ptr[col] = 0.55;
-			}
-		}
-	}*/
-	cv::namedWindow("Teste", cv::WINDOW_AUTOSIZE);
+	/* Size mask*/
+	int size_mask = mask_array.size();
 	/* Compute bertalmio for each stage */
 	for (int stage = 0; stage < total_stages; stage++) {
-		cv::imshow("Teste", output_array);
 		
 		/* Grab data */
 		int total_iter = total_iters[stage];
@@ -136,18 +129,16 @@ void perform_bertalmio_pde_inpainting_0(
 					diffuse_coefs = 1 / (1 + temp);
 				}
 				cv::Laplacian(output_array, image_laplacian, -1);
-				for (int row = 0; row < output_array.rows; row++) {
-					float* output_array_ptr = output_array.ptr<float>(row);
-					float* diffuse_coefs_ptr = diffuse_coefs.ptr<float>(row);
-					float* image_laplacian_ptr = image_laplacian.ptr<float>(row);
-					logical_type* mask_array_ptr = mask_array.ptr<logical_type>(row);
-					for (int col = 0; col < output_array.cols; col++) {
-						if (mask_array_ptr[col] != 0) {
-							output_array_ptr[col] +=
-								delta_t * (diffuse_coefs_ptr[col] * image_laplacian_ptr[col]);
-						}
-					}
+				
+				for (int cont = 0; cont < size_mask; cont++) {
+					Coord coord = mask_array.at(cont);
+					int row = coord.i;
+					int col = coord.j;
+
+					output_array.at<float>(row, col) +=
+						delta_t * (diffuse_coefs.at<float>(row, col) * image_laplacian.at<float>(row, col));
 				}
+
 			}
 
 			/* Perform inpainting */
@@ -159,30 +150,49 @@ void perform_bertalmio_pde_inpainting_0(
 				cv::Laplacian(output_array, image_laplacian, -1);
 				cv::Sobel(image_laplacian, image_laplacian_grad_row, -1, 0, 1);
 				cv::Sobel(image_laplacian, image_laplacian_grad_col, -1, 1, 0);
-				for (int row = 0; row < output_array.rows; row++) {
-					logical_type* mask_array_ptr = mask_array.ptr<logical_type>(row);
-					float* image_iso_norm_ptr = image_iso_norm.ptr<float>(row);
-					float* image_iso_row_ptr = image_iso_row.ptr<float>(row);
-					float* image_iso_col_ptr = image_iso_col.ptr<float>(row);
-					float* image_laplacian_grad_row_ptr = image_laplacian_grad_row.ptr<float>(row);
-					float* image_laplacian_grad_col_ptr = image_laplacian_grad_col.ptr<float>(row);
-					float* output_array_ptr = output_array.ptr<float>(row);
-					for (int col = 0; col < output_array.cols; col++) {
-						if ((mask_array_ptr[col] != 0) && (image_iso_norm_ptr[col] != 0)) {
-							output_array_ptr[col] -= delta_t * (
-								image_iso_row_ptr[col] * image_laplacian_grad_row_ptr[col] +
-								image_iso_col_ptr[col] * image_laplacian_grad_col_ptr[col]) /
-								image_iso_norm_ptr[col];
-							//std::cout << image_iso_norm_ptr[col] << std::endl;
-							output_array_ptr[col] = (output_array_ptr[col] > 1.0f) ? 1 : output_array_ptr[col];
-							output_array_ptr[col] = (output_array_ptr[col] < 0.0f) ? 0 : output_array_ptr[col];
-							//std::cout << output_array_ptr[col] << std::endl;
-						}
-					}
+
+				for (int cont = 0; cont < size_mask; cont++) {
+					Coord coord = mask_array.at(cont);
+					int row = coord.i;
+					int col = coord.j;
+					if (image_iso_norm.at<float>(row, col) != 0) {
+						output_array.at<float>(row, col) -= delta_t * (
+							image_iso_row.at<float>(row, col) * image_laplacian_grad_row.at<float>(row, col) +
+							image_iso_col.at<float>(row, col) * image_laplacian_grad_col.at<float>(row, col)) /
+							image_iso_norm.at<float>(row, col);
+						output_array.at<float>(row, col) = (output_array.at<float>(row, col) > 1.0f ? 1 : output_array.at<float>(row, col));
+						output_array.at<float>(row, col) = (output_array.at<float>(row, col) < 0.0f ? 0 : output_array.at<float>(row, col));
+					}			
 				}
 			}
 		}
 	}
-	cv::waitKey(0);
 }
 
+
+
+/*
+	Save the inpainting domain to dinamic vector
+*/
+std::vector<Coord> create_mask(cv::Mat& mask) {
+	std::vector<Coord> mask_data;
+	for (int i = 1; i < mask.rows - 1; i++) {
+		for (int j = 1; j < mask.cols - 1; j++) {
+			if (mask.at<cv::Vec3b>(i, j)[0] != 0) { //BLUE GREEN RED --> white (255,255,255) 
+				Coord xy;
+				xy.i = i;
+				xy.j = j;
+				xy.color = PIXEL_WHITE;
+				mask_data.push_back(xy);
+			}
+			else if (mask.at<cv::Vec3b>(i, j)[0] == 0 && mask.at<cv::Vec3b>(i, j)[2] == 255) { //BLUE GREEN RED --> red (0,0,255) 
+				Coord xy;
+				xy.i = i;
+				xy.j = j;
+				xy.color = PIXEL_RED;
+				mask_data.push_back(xy);
+			}
+		}
+	}
+	return mask_data;
+}
